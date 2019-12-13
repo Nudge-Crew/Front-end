56de8d22bea33644dcc194f31c03978f126c3b5f
Berg,Luuk L.L.M. van den <l.vandenberg@student.fontys.nl>
2019-12-13 11:11:14 +0100

    added pi chart with test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { dataset } from './data'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nudge-crew';
+  dataset: any[];
+  view: any[] = [700,400]
+
+   // options
+   gradient: boolean = true;
+   showLegend: boolean = true;
+   showLabels: boolean = true;
+   isDoughnut: boolean = false;
+   legendPosition: string = 'below';
+ 
+   colorScheme = {
+     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+   };
+ 
+   constructor() {
+     Object.assign(this, { dataset });
+   }
+ 
+   onSelect(data): void {
+     console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+   }
+ 
+   onActivate(data): void {
+     console.log('Activate', JSON.parse(JSON.stringify(data)));
+   }
+ 
+   onDeactivate(data): void {
+     console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+   }
+
 }
+
